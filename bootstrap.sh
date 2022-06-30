48ae7674c33869a24bfc0f31f859b3e573cff486
Donovan Said <donovan.s.said@gmail.com>
2022-06-30 13:44:02 +0100

    Added brew to bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,12 @@ set -e
 # A Shell script used to bootstrap OSX in preperation of the Ansible playbook.
 
 ################################################################################
+# Brew Installation                                                            # 
+################################################################################
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+################################################################################
 # Ansible Execution                                                            # 
 ################################################################################
 
