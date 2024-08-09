80381958034d79dbc7b06033a567f19955179ce2
donovan-said <donovan.s.said@gmail.com>
2024-08-09 12:21:51 +0100

    Moved brew and vscode installations to brewfile.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,20 @@ set -e
 # A Shell script used to bootstrap OSX in preparation of the Ansible playbook.
 
 if [[ $(command -v brew) == "" ]]; then
-    echo "Installing Hombrew"
+    echo ">> Installing Hombrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    echo "Updating Homebrew"
+    echo ">> Homebrew is installed, performing update"
     brew update
 fi
 
-brew install ansible
+if [[ $(command -v ansible) == "" ]]; then
+  echo ">> Installing Ansible"
+  brew install ansible
+else
+  echo ">> Ansible is installed, performing upgrade"
+  brew upgrade ansible
+fi
 
 ansible-playbook playbook.yml -i inventory.ini
 
